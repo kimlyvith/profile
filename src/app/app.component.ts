b7a22850145cdd9d99ab829fb5c7104e68886497
Kimly <kimlyvithkimly@gmail.com>
2022-05-19 13:04:01 +0700

    add profile

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portfolio';
+  authorName='Borin Min';
+  bod='0000-00-00';
+  address='Cambodia';
+}
